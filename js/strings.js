51c8106dd6e87a3dd1063a00489e25c49449aca3
Hiba-Almade <hibaalmade998@gmail.com>
2022-05-19 20:50:48 +0300

    create strings modules and handler

---
+++ js/strings.js
@@ -0,0 +1,8 @@
+class Strings {
+    constructor(data = {}) {
+      // DONT DELETE THESE DATA OBJECTS 
+      this.share = data.share || 'Share the App via..'; 
+      this.message = data.message || 'Scan to share the QR code.'; 
+      //ADD NEW ONES BELOW
+    }
+  }
