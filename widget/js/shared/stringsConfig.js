acd7d205621f982eca1c8e4979e8e9eec02d4762
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-05-22 17:49:01 +0300

    add files for build strings page in the buildfire standard method

---
+++ widget/js/shared/stringsConfig.js
@@ -0,0 +1,20 @@
+const stringsConfig = {
+	screenOne: {
+		title: "Share Plugin",
+		subtitle: "",
+		labels: {
+			share: {
+				title: "Share Button",
+				placeholder: "Share the App Via...",
+				maxLength: 50,
+				defaultValue: "Share the App Via..."
+			},
+            message: {
+				title: "Share Messages",
+				placeholder: "Scanto share with QR Code.",
+				maxLength: 50,
+				defaultValue: "Scanto share with QR Code."
+			},
+		}
+	}
+};
